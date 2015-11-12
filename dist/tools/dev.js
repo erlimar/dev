cc67fcb585594aed26edd3fb1f71826897d4e6c9
Erlimar Silva Campos <erlimar@gmail.com>
2015-11-12 02:58:35 -0200

    Migrated to ES6

--- dist/tools/dev.js
+++ dist/tools/dev.js
@@ -3,45 +3,131 @@
 
 /* global process, __filename, __dirname */
 
-var _verbose = console.info,
+/* @TODO: Use Gulp to build system */
+
+let util = require('util'),
+    path = require('path'),
+
+    _print = console.log,
+    _verbose = console.info,
     _debug = console.log,
     _warning = console.warn,
     _error = console.error;
-        
-const ERROR_CODE = {
-    DEVCOM_NOTFOUND: 1
-};
 
+const TOOL_TITLE = 'E5R Tools for Development Team';
+const TOOL_VERSION = '0.1.0-alpha';
+const TOOL_COPYRIGHT = 'Copyright (c) E5R Development Team. All rights reserved.';
+const ERROR_CODE_DEVCOM_NOTINFORMED = 9001;
+
+/**
+ * Builtin base class
+ * 
+ * @TODO: Move to `src/builtin.js`
+ */
 class Builtin {
-    run(args){
-        
+
+    /**
+     * Name of the Built-in
+     */
+    get name() {
+        throw new Error('Built-in [name] not implemented.');
+    }
+    
+    /**
+     * Run the builtin command
+     * 
+     * @param {Array} args - Arguments of command
+     */
+    run(args) {
+        throw new Error('Built-in [run()] not implemented.');
     }
 }
 
 /**
  * E5R Development Tool
+ * 
+ * @TODO: Move to `src/devtool.js`
  */
 class DevTool {
     
     /**
      * @constructor
      * 
-     * @param {Array} builtins - List of built-in function.
+     * @param {Array} builtins - List of built-in functions
      */
     constructor(builtins) {
-        _debug('DevTool created in "' + __filename +'"');
-        
-        if(!Array.isArray(builtins)){
+        if (!util.isArray(builtins)) {
             throw new Error('Invalid @param builtins. Must be an array builtins.');
         }
-        
+
+        this._args = process.argv.slice(2);
+        this._name = path.parse(__filename).name;
+        this._cmd = (this._args.shift() || '').toLowerCase();
         this._builtin = new Object;
-        
-        // Registry built-in
-        for(var b in builtins){
-            _debug('b =>', builtins[b]);
+
+        this.registry(builtins);
+        this.run();
+    }
+    
+    /**
+     * Show usage text
+     */
+    usage() {
+        _print('%s v%s', TOOL_TITLE, TOOL_VERSION);
+        _print('%s', TOOL_COPYRIGHT);
+        _print();
+        _print('Usage: %s [devcom] [options]', this._name);
+        _print();
+        _print('DevCom:');
+
+        let devcomNames = Object.getOwnPropertyNames(this._builtin);
+        for (let d in devcomNames) {
+            let devcom = this._builtin[devcomNames[d]];
+            _print('  %s', devcom.name);
+        }
+
+        _print('  %s', '???');
+        _print();
+        _print('Options:');
+        _print('  ???');
+    }
+    
+    /**
+     * Exit process tool
+     * 
+     * @param {int} code - Exit code
+     */
+    exit(code) {
+        process.exit(code);
+    }
+    
+    /**
+     * Registry Built-in Functions.
+     * 
+     * @param {Array} builtins - List of built-in functions
+     */
+    registry(builtins) {
+        for (let b in builtins) {
             this.builtin = builtins[b];
         }
+    }
+    
+    /**
+     * Run the tool
+     */
+    run() {
+        if (!this._cmd) {
+            this.usage();
+            this.exit(ERROR_CODE_DEVCOM_NOTINFORMED);
+        }
+
+        let devcom = this.builtin[this._cmd];
+
+        if (!devcom) {
+            throw new Error('DEVCOM [' + this._cmd + '] not found!');
+        }
+
+        devcom.run(this._args);
     }
     
     /**
@@ -54,72 +140,82 @@ class DevTool {
     /**
      * Built-in Functions Setter
      */
-    set builtin(value) {
-        if (typeof (value) != 'function') {
-            throw new Error('Invalid Built-in Function!');
+    set builtin(BuiltinType) {
+        if (typeof (BuiltinType) != 'function') {
+            throw new Error('Invalid Built-in type');
         }
-        
-        var name = value.name.toLowerCase();
 
-        if (this._builtin.hasOwnProperty(name)) {
-            throw new Error('Built-in Function [' + name + '] already exists.');
+        let instance = new BuiltinType;
+
+        if (!(instance instanceof Builtin)) {
+            throw new Error('Invalid Built-in type inheritance.');
         }
-        
-        Object.defineProperty(this._builtin, name, {
-            value: value
+
+        if (this._builtin.hasOwnProperty(instance.name)) {
+            throw new Error('Built-in Function [' + instance.name + '] already exists.');
+        }
+
+        Object.defineProperty(this._builtin, instance.name, {
+            value: instance
         })
-    }
-    
-    /**
-     * Run the DEVCOM (Development Command) called of the shell
-     * 
-     * @param {string} devcom - Development Command
-     * @param {Array} args - Shell arguments
-     */
-    run (devcom, args) {
-        var cmd = this.builtin[devcom];
-
-        if (!cmd) {
-            _verbose('DEVCOM [' + devcom + '] not found!');
-        }
     }
 }
 
+/**
+ * Built-in `install` command
+ * 
+ * Install the E5R Development Tool on the user home
+ * 
+ * @TODO: Move to `src/install.js`
+ */
+class Install extends Builtin {
+    get name() {
+        return 'install';
+    }
 
+    run(args) {
+        _debug('Install built-in is running...');
+        
+        // 1> Download de url://dist/registry.json para %HOME%\.dev\registry.json
+        //   Este arquivo contém os URL's com plugins (DEVCOM - Development Command),
+        //   no seguinte formato:
+        //   {
+        //       "default": {
+        //           "type": "GitHub",
+        //           "owner": "e5r",
+        //           "repository": "dev",
+        //           "branch": "develop",
+        //           "path": "dist"
+        //           },
+        //           "sampleUrlForRegistryFile": "https://raw.githubusercontent.com/e5r/dev/develop/dist/registry.json"
+        //       },
+        //       "default-url": {
+        //           "type": "URL",
+        //           "repository": "http://dev.mycompany.com/plugins",
+        //           "sampleUrlForRegistryFile": "http://dev.mycompany.com/plugins/registry.json"
+        //       }
+        //   }
+        
+        // 2> Download de url://dist/bin/dev.{cmd,ps1} (Windows) para %HOME%\.dev\bin\dev.{cmd,ps1}
+        //   Esses arquivos (dev.cmd e dev.ps1) devem somente repassar os argumentos para o comando
+        //   nodeJS [jsengine.exe dev.js *ARGS*]
+        //   - Inclui %HOME%\.dev\bin ao %PATH%
+        //   - Ver o uso de arquivo *.CMD & *.PS1 para propagação de %PATH%.
+        
+        // 3> Download de url://dist/tools/dev.js par %HOME%\.dev\tools\dev.js
+        
+        // 4> Apagar este arquivo (%HOME%\.dev\tools\install.js)
 
-// 1> Download de url://dist/registry.json para %HOME%\.dev\registry.json
-//   Este arquivo contém os URL's com plugins (DEVCOM - Development Command),
-//   no seguinte formato:
-//   {
-//       "default": {
-//           "type": "GitHub",
-//           "owner": "e5r",
-//           "repository": "dev",
-//           "branch": "develop",
-//           "path": "dist"
-//           },
-//           "sampleUrlForRegistryFile": "https://raw.githubusercontent.com/e5r/dev/develop/dist/registry.json"
-//       },
-//       "default-url": {
-//           "type": "URL",
-//           "repository": "http://dev.mycompany.com/plugins",
-//           "sampleUrlForRegistryFile": "http://dev.mycompany.com/plugins/registry.json"
-//       }
-//   }
+    }
+}
 
-// 2> Download de url://dist/bin/dev.{cmd,ps1} (Windows) para %HOME%\.dev\bin\dev.{cmd,ps1}
-//   Esses arquivos (dev.cmd e dev.ps1) devem somente repassar os argumentos para o comando
-//   nodeJS [jsengine.exe dev.js *ARGS*]
-//   - Inclui %HOME%\.dev\bin ao %PATH%
-//   - Ver o uso de arquivo *.CMD & *.PS1 para propagação de %PATH%.
+new DevTool([
+    Install
+]);
 
-// 3> Download de url://dist/tools/dev.js par %HOME%\.dev\tools\dev.js
+/* 
 
-// 4> Apagar este arquivo (%HOME%\.dev\tools\install.js)
-
-
-
-/* ~file: /tools/dev.js
+@NOTES:
     
 O primeiro argumento sempre deve ser o %DEVCOM%, e existe em:
 %HOME%\.dev\tools\devcom\*.js
@@ -143,14 +239,3 @@ DEVCOM padrões:
     * update [url for registry.json] -> Faz um merge do `registry.json` atual com o baixado da url
     Todos os registros no remoto serão adicionados ou substituirão os existentes localmente
 */
-
-class Install extends Builtin {
-    run (args) {
-        _debug('Install built-in is running...');
-    }
-    
-    
-}
-
-new DevTool([])
-    .run('install', []);
