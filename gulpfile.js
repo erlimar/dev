99563b2b17d1c79160e29c251fb82f7f316f827e
Erlimar Silva Campos <erlimar.campos@basis.com.br>
2016-04-06 11:23:14 -0300

    Added test entry for gulp, added scripts for npm (only windows)

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,11 @@ var E5R_DIR = '.dev';
 var E5R_INSTALL_DIR = path.join(os.homedir(), E5R_DIR, 'lib', 'node_modules');
 var E5R_LIB_NAME = 'e5r-dev.js';
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
     return del('dist/*');
 });
 
-gulp.task('dist', ['clean'], function () {
+gulp.task('dist', ['clean'], function() {
     gulp.src(jsconfig.files.concat(['!src/globals.js', '!dist/e5r-dev.js']))
         .pipe(concat.header(bannerTxt, { pkg: pkg }))
         .pipe(concat(E5R_LIB_NAME))
@@ -36,4 +36,8 @@ gulp.task('install', [], function() {
         .pipe(concat(E5R_LIB_NAME))
         .pipe(concat.header(headerTxt, { pkg: pkg }))
         .pipe(gulp.dest(E5R_INSTALL_DIR));
+});
+
+gulp.task('test', [], function() {
+    console.log('#TODO: Test not implemented!');
 });
