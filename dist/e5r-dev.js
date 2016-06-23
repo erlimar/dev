4c3199b942341ce3535ceb8ce86850f40f2a2a50
Erlimar Silva Campos <erlimar.campos@basis.com.br>
2016-06-23 18:09:26 -0300

    Prepare to release 0.4.1

--- dist/e5r-dev.js
+++ dist/e5r-dev.js
@@ -13,7 +13,7 @@
 const TOOL_TITLE = 'E5R Tools for Development Team';
 
 /** @constant {string} */
-const TOOL_VERSION = '0.4.0';
+const TOOL_VERSION = '0.4.1';
 
 /** @constant {string} */
 const TOOL_COPYRIGHT = '(c) E5R Development Team. All rights reserved.';
@@ -2323,5 +2323,7 @@ if (!module.parent && module.filename === __filename) {
     if(lib.devToolDefaultInstance.exitCode === 0){
         lib.devToolDefaultInstance.run();
     }
-    lib.devToolDefaultInstance.exit();
+    if(lib.devToolDefaultInstance.exitCode !== 0){
+        lib.devToolDefaultInstance.exit();
+    }
 }
