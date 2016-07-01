6e53cb26e1e352f4204d13ca6876856f6cd2b1dd
Erlimar Silva Campos <erlimar.campos@basis.com.br>
2016-07-01 12:26:10 -0300

    Fixed error on create e5r directory

--- dist/e5r-dev.js
+++ dist/e5r-dev.js
@@ -2016,7 +2016,7 @@ var lib =
  * Setup the E5R Development Tool on the user home
  */
 class Setup extends lib.DevCom {
-    
+
     /**
      * Run the `setup` built-in command
      * 
@@ -2025,7 +2025,7 @@ class Setup extends lib.DevCom {
      */
     run(devTool, options) {
         lib.printf('Set-up E5R Tools for Development Team...');
-        
+
         // 1> Make directory structure
         [
             lib.devHome.root,
@@ -2035,29 +2035,27 @@ class Setup extends lib.DevCom {
             lib.devHome.cmd,
             lib.devHome.doc
         ].map(path => {
-            if (!lib.fileExists(path)) {
-                _fs.mkdirSync(path);
-            }
+            lib.mkdir(path);
         });
-        
+
         // 2> Download `registry.json`
         lib.downloadSync(
             _url.resolve(TOOL_DEFAULT_REGISTRY_URL, TOOL_REGISTRY_FILE),
             _path.resolve(lib.devHome.root, TOOL_REGISTRY_FILE)
-            );
-        
+        );
+
         // 3> Add /bin to PATH
         /** @todo: Ver o uso de arquivo *.CMD & *.PS1 para propagação de %PATH%. */
         /** @todo: Ver FLAG de tipo de sessão (PS1, CMD, SH) */
         lib.addPathToEnvironmentPath(lib.devHome.bin, devTool);
-        
+
         // 4> InstalL binary
         let registry = lib.require('cmd://registry');
 
         registry.run(devTool, parseArgOptions([
             'get-binaries'
         ]));
-        
+
         // 5> Show completed info
         lib.printf('Set-up completed!');
     }
--- src/setup.js
+++ src/setup.js
@@ -5,7 +5,7 @@
  * Setup the E5R Development Tool on the user home
  */
 class Setup extends lib.DevCom {
-    
+
     /**
      * Run the `setup` built-in command
      * 
@@ -14,7 +14,7 @@ class Setup extends lib.DevCom {
      */
     run(devTool, options) {
         lib.printf('Set-up E5R Tools for Development Team...');
-        
+
         // 1> Make directory structure
         [
             lib.devHome.root,
@@ -24,29 +24,27 @@ class Setup extends lib.DevCom {
             lib.devHome.cmd,
             lib.devHome.doc
         ].map(path => {
-            if (!lib.fileExists(path)) {
-                _fs.mkdirSync(path);
-            }
+            lib.mkdir(path);
         });
-        
+
         // 2> Download `registry.json`
         lib.downloadSync(
             _url.resolve(TOOL_DEFAULT_REGISTRY_URL, TOOL_REGISTRY_FILE),
             _path.resolve(lib.devHome.root, TOOL_REGISTRY_FILE)
-            );
-        
+        );
+
         // 3> Add /bin to PATH
         /** @todo: Ver o uso de arquivo *.CMD & *.PS1 para propagação de %PATH%. */
         /** @todo: Ver FLAG de tipo de sessão (PS1, CMD, SH) */
         lib.addPathToEnvironmentPath(lib.devHome.bin, devTool);
-        
+
         // 4> InstalL binary
         let registry = lib.require('cmd://registry');
 
         registry.run(devTool, parseArgOptions([
             'get-binaries'
         ]));
-        
+
         // 5> Show completed info
         lib.printf('Set-up completed!');
     }
