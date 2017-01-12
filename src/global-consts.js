fe9e9c51d8e963aac61f1369919386527692f447
Erlimar Silva Campos <erlimar@gmail.com>
2017-01-12 20:53:13 -0200

    Prepare to release 0.12.0

--- src/global-consts.js
+++ src/global-consts.js
@@ -2,7 +2,7 @@
 const TOOL_TITLE = 'E5R Tools for Development Team';
 
 /** @constant {string} */
-const TOOL_VERSION = '0.11.0';
+const TOOL_VERSION = '0.12.0';
 
 /** @constant {string} */
 const TOOL_COPYRIGHT = '(c) E5R Development Team. All rights reserved.';
