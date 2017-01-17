3f4e959e991b4bd8852bd596dc43170fda2ba7c4
Erlimar Silva Campos <erlimar@gmail.com>
2017-01-17 18:31:00 -0200

    Regression to common JS notation

--- scripts/devutils.js
+++ scripts/devutils.js
@@ -2,29 +2,33 @@
 // Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
 function expandsToGlobal(obj) {
-    for (let p in obj) global[p] = obj[p]
+    for (let p in obj) {
+        global[p] = obj[p];
+    }
 }
 
 let loaded = []
 
-module.exports.requireGlobal = (m) => {
-    if (!Array.isArray(m)) m = [m]
-
-    m.map(moduleName => {
-        if (loaded.indexOf(moduleName) < 0) {
-            expandsToGlobal(require('../src/' + moduleName))
-            loaded.push(moduleName)
-        }
-    })
-
-    return module.exports
-}
-
-module.exports.ensureNode = () => {
-    // NodeJS >= 7.0.0 is required
-    if (process.versions.node.split('.')[0] < '7') {
-        throw new Error('NodeJS >= 7.0.0 is required!')
+module.exports.requireGlobal = function (m) {
+    if (!Array.isArray(m)) {
+        m = [m];
     }
 
-    return module.exports
+    m.map(function (moduleName) {
+        if (loaded.indexOf(moduleName) < 0) {
+            expandsToGlobal(require('../src/' + moduleName));
+            loaded.push(moduleName);
+        }
+    });
+
+    return module.exports;
+}
+
+module.exports.ensureNode = function () {
+    // NodeJS >= 7.0.0 is required
+    if (process.versions.node.split('.')[0] < '7') {
+        throw new Error('NodeJS >= 7.0.0 is required!');
+    }
+
+    return module.exports;
 }
